2efc4872961a2316cd0f08d93f09a5013031a0b2
Anni Hakanen <hakanen.anni@gmail.com>
2018-11-28 22:11:35 +0200

    Update index.js

--- Unicafe/unicafe_palaute/src/index.js
+++ Unicafe/unicafe_palaute/src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Otsikko = (props) => {
+    return (
+        <h1>{props.otsikko}</h1>
+    )
+}
+const Sisalto = (props) => {
+    return (
+        <div>
+            <p>{props.teksti1}: {props.aania_hyva}</p>
+            <p>{props.teksti2}: {props.aania_neutraali}</p>
+            <p>{props.teksti3}: {props.aania_huono}</p>
+        </div>
+    )
+}
+const Painike = (props) => {
+    return (
+        <button onClick={props.toiminto}>{props.teksti}</button>
+    )
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hyva: 0,
+            neutraali: 0,
+            huono: 0
+        }
+    }
+
+    lisaaAaniHyva = () => {
+        this.setState({ hyva: this.state.hyva + 1 })
+    }
+    lisaaAaniNeutraali = () => {
+        this.setState({ neutraali: this.state.neutraali + 1 })
+    }
+    lisaaAaniHuono = () => {
+        this.setState({ huono: this.state.huono + 1 })
+    }
+
+    render() {
+        return (
+            <div>
+                <Otsikko otsikko={"Anna palautetta"} />
+
+                <Painike toiminto={this.lisaaAaniHyva} teksti="Hyvä"></Painike>
+                <Painike toiminto={this.lisaaAaniNeutraali} teksti="Neutraali"></Painike>
+                <Painike toiminto={this.lisaaAaniHuono} teksti="Huono"></Painike>
+
+                <Otsikko otsikko={"Statistiikka"} />
+
+                <Sisalto
+                    teksti1={"Hyvä"} aania_hyva={this.state.hyva}
+                    teksti2={"Neutraali"} aania_neutraali={this.state.neutraali}
+                    teksti3={"Huono"} aania_huono={this.state.huono} />
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
