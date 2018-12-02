3db073687f1251f5a527a2dbe2af7e10fd894c96
Anni Hakanen <hakanen.anni@gmail.com>
2018-12-02 19:33:12 +0200

    Tehtava1.14

--- Anekdootit/anekdootit_sovellus/src/index.js
+++ Anekdootit/anekdootit_sovellus/src/index.js
@@ -2,43 +2,75 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = (props) => {
-    // console.log(props)
-    const { handleClick, teksti } = props
-    return (
-        <button onClick={handleClick}>
-            {teksti}
-        </button>
-    )
+  // console.log(props)
+  const { handleClick, teksti } = props
+  return (
+    <button onClick={handleClick}>
+      {teksti}
+    </button>
+  )
+}
+const Otsikko = (props) => {
+  return (
+    <h1>{props.otsikko}</h1>
+  )
 }
 class App extends React.Component {
+
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      taulukko: [0, 0, 0, 0, 0, 0, 0],
+      paras: 0
     }
   }
 
   arvoAnekdootti = (min, max) => {
-      // min = 1 max = 6
-    //   const arvottuLuku = Math.floor(Math.random() * (6-1) +1) +1;
-    //   console.log("arvottu" , arvottuLuku)
-      return () => {
-          this.setState({selected: Math.floor(Math.random() * (max - min + 1) ) + min})
-          console.log(this.state.selected)
+    return () => {
+      this.setState({ selected: Math.floor(Math.random() * (max - min + 1)) + min })
+    }
+  }
+  lisaaAani = (selected) => {
+    this.laskeAanet()
+    return () => {
+      const kopio = [...this.state.taulukko]
+      kopio[selected] += 1
+      this.setState({
+        taulukko: kopio
+      })
+    }
+  }
+  laskeAanet = () => {
+    for (let i = 0; i < this.state.taulukko.length; i++) {
+      if (this.state.taulukko[i] > this.state.taulukko[this.state.paras]) {
+        this.setState({
+          paras: i
+        }
+        )
       }
+    }
+    return (this.state.taulukko[this.state.paras])
   }
 
   render() {
+    // const aanet = () => this.state.taulukko.join(' ')
     return (
       <div>
-        <Button handleClick={this.arvoAnekdootti(0,5)} teksti="Arvo"></Button>
+        <Otsikko otsikko="Anekdoottilotto" />
+        <Button handleClick={this.arvoAnekdootti(1, 6)} teksti="Arvo uusi anekdootti"></Button>
         <p>{this.props.anecdotes[this.state.selected]}</p>
+        <Button handleClick={this.lisaaAani(this.state.selected)} teksti="Äänestä tätä"></Button>
+        {/* <div><p>{aanet()}</p></div> */}
+        <div><p>Eniten ääniä saanut anekdootti:</p> 
+        <p>{this.props.anecdotes[this.state.paras]}</p></div>
       </div>
     )
   }
 }
 
 const anecdotes = [
+  '',
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
