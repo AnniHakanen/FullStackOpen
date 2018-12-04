21f63f7559c8a8d3d547fd08b48520d09a289429
Anni Hakanen <hakanen.anni@gmail.com>
2018-12-05 00:40:18 +0200

    Tehtava 2.5

--- tehtava1/src/index.js
+++ tehtava1/src/index.js
@@ -1,55 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Kurssit from './components/Kurssit'
 
-const Kurssit = (props) => {
-    return (
-        <div>
-            {props.kurssiluettelo.map(yksittainenKurssi =>
-                <Kurssi key={yksittainenKurssi.id} kurssi={yksittainenKurssi} osat={yksittainenKurssi.osat} />)}
-        </div>
-    )
-}
-const Kurssi = (props) => {
-    return (
-        <div>
-            <Otsikko kurssi={props.kurssi} />
-            {/* <h1>{props.kurssi.nimi}</h1> */}
-            <Sisalto osat={props.osat} />
-            <Yhteensa osat={props.osat} />
-        </div>)
-}
-const Otsikko = (props) => {
-    return (
-        <h1>{props.kurssi.nimi}</h1>
-    )
-}
-const Sisalto = (props) => {
-    return (
-        <ul>
-            {/* <Osa osa={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia} />
-            <Osa osa={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia} />
-            <Osa osa={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia} /> */}
-            {props.osat.map(osa => <Osa key={osa.id} osa={osa.nimi} tehtavia={osa.tehtavia} />)}
-        </ul>
-    )
-}
-const Osa = (props) => {
-    return (
-        <li>
-            {props.osa}, tehtäviä: {props.tehtavia}
-        </li>
-    )
-}
-const Yhteensa = (props) => {
-    // console.log('REDUCE')
-    var tehtavienSumma = props.osat.reduce(function (sum, osa) {
-        return sum + osa.tehtavia
-    }, 0)
-    // console.log(tehtavienSumma)
-    return (
-        <p>Yhteensä {tehtavienSumma} tehtävää</p>
-    )
-}
 const App = () => {
     const kurssit = [
         {
@@ -99,9 +51,6 @@ const App = () => {
     return (
         <div>
             <h1>Opetusohjelma</h1>
-            {/* <Otsikko kurssi={kurssi} />
-            <Sisalto osat={kurssi.osat} />
-            <Yhteensa osat={kurssi.osat} /> */}
             <Kurssit kurssiluettelo={kurssit} />
         </div>
     )
